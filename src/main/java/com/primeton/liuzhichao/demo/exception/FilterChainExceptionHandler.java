ed0a63de7979fdace7b2beee39c9a880adc3068b
liu-zc <email>
2020-09-27 14:48:00 +0800

    2020_09_27

---
+++ src/main/java/com/primeton/liuzhichao/demo/exception/FilterChainExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.primeton.liuzhichao.demo.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName FilterChainExceptionHandler
+ * @Description TODO
+ * @Author liuzhichao
+ * @Date 2020/9/27 13:45
+ * @Version 1.0
+ * 用于捕获拦截器中的异常，需要将此类挂载到Sercity的过滤器链中
+ */
+@Slf4j
+@Component
+public class FilterChainExceptionHandler extends OncePerRequestFilter {
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            log.error("Spring Security Filter Chain Exception:", e);
+            resolver.resolveException(request, response, null, e);
+        }
+    }
+
+}
